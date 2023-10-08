87fead67852fb4dc35c9186f8ae3cb138fdd1023
John Wason <wason@wasontech.com>
2023-10-08 10:29:41 -0400

    Add github actions test

--- test/tiny_test/tiny_client_ci_test.py
+++ test/tiny_test/tiny_client_ci_test.py
@@ -11,7 +11,7 @@ print(examples_build_dir)
 py_dir = repo_dir / "examples" / "tiny_client"
 
 service = subprocess.Popen([sys.executable, py_dir / "tiny_client_service.py"], cwd=py_dir, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-time.sleep(0.5)
+time.sleep(2)
 
 subprocess.check_call([examples_build_dir / "robotraconteurlite_tiny_client"], cwd=examples_build_dir)
 
--- test/tiny_test/tiny_service_ci_test.py
+++ test/tiny_test/tiny_service_ci_test.py
@@ -12,7 +12,7 @@ print(examples_build_dir)
 py_dir = repo_dir / "examples" / "tiny_service"
 
 service = subprocess.Popen([examples_build_dir / "robotraconteurlite_tiny_service"], cwd=examples_build_dir, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-time.sleep(0.5)
+time.sleep(2)
 
 
 
